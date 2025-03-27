8f5723d8aea796d79eb1d0aebeda9b49a67376a2
Anthony Laplane <anthony.laplane@gmail.com>
2025-03-27 18:59:05 +0100

    live 2/7

---
+++ jeux.php
@@ -0,0 +1,27 @@
+<?php 
+
+require_once "libs/game.php";
+require_once "templates/header.php";
+
+$games = getAllGames();
+?>
+
+<section class="text-gray-400 bg-gray-900 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col">
+            <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+                <h1 class="sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0">Les derniers jeux</h1>
+            </div>
+        </div>
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+
+            <?php foreach ($games as $index=>$game): ?>
+                <?php require "templates/_game_item.php"; ?>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
+</section>
+
+
+<?php require_once "templates/footer.php"; ?>
---
+++ libs/game.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function getAllGames():array
+{
+    $games= [
+        ["name" => "Spider-Man 2", "description" => "Incarnez le célèbre super-héros dans une nouvelle aventure pleine d'action. Balancez-vous à travers la ville..."],
+        ["name" => "Like a Dragon: Infinite Wealth", "description" => "Explorez un monde ouvert rempli de possibilités et de défis. Incarnez un héros et affrontez..."],
+        ["name" => "Starfield", "description" => "Partez à la découverte des étoiles dans ce jeu de rôle et d'action futuriste. Explorez des planètes inconnues, interagissez avec des factions spatiales..."],
+    ];
+    return $games;
+}
